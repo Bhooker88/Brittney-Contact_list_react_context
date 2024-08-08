b07360185db92aff1457227407adf46db65cb404
Brittney Hooker <139884664+Bhooker88@users.noreply.github.com>
2024-08-08 15:14:33 +0000

    Update configuration for correct paths

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,43 +1,15 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const webpack = require('webpack');
-const dotenv = require('dotenv');
-
-// Load environment variables from .env file
-dotenv.config();
-
-module.exports = {
-  mode: 'production',
-  entry: './src/js/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public'),
-    publicPath: '/',
-  },
-  devtool: 'source-map',
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
-    new webpack.DefinePlugin({
-      'process.env.BASENAME': JSON.stringify(process.env.BASENAME || ''),
-    }),
-  ],
-};
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+const Dotenv = require('dotenv-webpack');
+module.exports = merge(common, {
+    mode: 'production',
+    output: {
+        publicPath: './'
+    },
+    plugins: [
+        new Dotenv({
+            safe: true,
+            systemvars: true
+        })
+    ]
+});
